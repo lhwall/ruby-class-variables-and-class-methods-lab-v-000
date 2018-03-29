b902f35a2603fb05b0735fc5aef5b7e14e8cadbb
Lucy Wall <lhwall@gmail.com>
2018-03-29 13:35:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,7 @@ class Song
 end 
   
   def self.artist_count
-    artist_count_count = {}
+    artist_count = {}
     @@artists.each do |artist|
       if artist_count.include?(artist) == false 
         artist_count[artist] = 1 
