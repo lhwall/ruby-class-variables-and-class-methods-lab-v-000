13fc2f6bcf106bec1824037f3ddbc1d7fbcb8e57
Lucy Wall <lhwall@gmail.com>
2018-03-29 13:34:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,4 +56,15 @@ class Song
   genre_count 
 end 
   
+  def self.artist_count
+    artist_count_count = {}
+    @@artists.each do |artist|
+      if artist_count.include?(artist) == false 
+        artist_count[artist] = 1 
+      else artist_count[artist] += 1 
+    end
+  end 
+  artist_count 
+end 
+  
 end
