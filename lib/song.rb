476d473f256854484bbc813aa13b082cbebf4ccd
Lucy Wall <lhwall@gmail.com>
2018-03-29 13:04:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,18 @@ class Song
  
   @@count = 0 
   
+  @@genres = []
+  
   def initialize
     @@count +=1 
+    
+    if @@genres.contains?()
   end
+  
+  def count
+    @@count
+  end 
+  
+  def genre
   
 end
